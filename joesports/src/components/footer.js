bc62b0450208d3c8572866895d259178b5e303e3
Joseph Pinangay <pinangayjoel@gmail.com>
2019-12-19 23:57:24 -0500

    Styling footer is done

--- joesports/src/components/footer.js
+++ joesports/src/components/footer.js
@@ -4,8 +4,10 @@ function Footer() {
   return (
     <div>
       <div className="footer">
-        <span>Created by Samuel Joseph Pinangay</span>
-        <span>API from https://newsapi.org/ </span>
+        <div className="footerSub">
+          <span>Created by Samuel Joseph Pinangay</span>
+          <span>API from https://newsapi.org/</span>
+        </div>
         <p>Copyright © 2019 all rights reserved</p>
       </div>
     </div>
